2e2b2e4d4d59049fc82e5b2a4725183b416c0768
Brian Berns <brianberns@gmail.com>
2025-02-24 14:25:52 -0500

    Added Queue.

--- Program.fs
+++ Program.fs
@@ -2,8 +2,21 @@
 
 module Program =
 
-    let stack =
-        Stack.empty Int.addition String.length
-            |> Stack.push "hello"
-            |> Stack.push "world"
-    printfn $"{stack.Contents}"
+    let test1 () =
+        let stack =
+            Stack.empty Int.addition String.length
+                |> Stack.push "hello"
+                |> Stack.push "world"
+        printfn $"{stack.Contents}"
+
+    let test2 () =
+        let str, q =
+            Queue.empty Int.addition String.length
+                |> Queue.enqueue "one"
+                |> Queue.enqueue "two"
+                |> Queue.enqueue "three"
+                |> Queue.dequeue
+                |> Option.get
+        printfn $"{str}, {q.Contents}"
+
+    test2 ()
---
+++ Queue.fs
@@ -0,0 +1,53 @@
+﻿namespace AnnotatedStack
+
+type Queue<'m, 'a> =
+    {
+        Monoid : Monoid<'m>
+        Front : Stack<'m, 'a>
+        Back : Stack<'m, 'a>
+    }
+
+    member this.Size =
+        this.Front.Size + this.Back.Size
+
+    member this.Measure =
+        this.Monoid.Append
+            this.Front.Measure
+            this.Back.Measure
+
+    member this.Contents =
+        this.Front.Contents @ this.Back.Contents
+
+module Queue =
+
+    let empty monoid annotate =
+        {
+            Monoid = monoid
+            Front = Stack.empty monoid annotate
+            Back = Stack.empty monoid annotate
+        }
+
+    let enqueue a q =
+        {
+            q with
+                Back = Stack.push a q.Back
+        }
+
+    let rec dequeue q =
+        match q.Front.Size, q.Back.Size with
+            | 0, 0 -> None
+            | 0, _ ->
+                dequeue {
+                    q with
+                        Front = Stack.reverse q.Back
+                        Back = q.Front
+                }
+            | _ ->
+                Stack.pop q.Front
+                    |> Option.map (fun (a, front) ->
+                        a, { q with Front = front })
+
+    let drop1 q =
+        match dequeue q with
+            | None -> q
+            | Some (_, q') -> q'
